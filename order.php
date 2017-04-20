c6a78a10572fa6a5d2dca594c88d2b64eec67ed4
neshka <neshka_mind@yahoo.com>
2017-04-20 23:56:49 +0100

    add table and vars

--- order.php
+++ order.php
@@ -8,23 +8,26 @@
 <?php
 $classic = $_POST['classic'];
 $glutenfree = $_POST['glutenfree'];
+$total = 0.50 * $classic + 0.75 * $glutenfree;
 
 echo <<<END
     <h2>Order summary</h2>
     <table border="1" cellpadding="10" cellspacing="0">
       <tr>
         <td>Classic £0.50</td>
-        <td></td>
+        <td>$classic</td>
       </tr>
       <tr>
         <td>Gluten Free £0.75</td>
-        <td></td>
+        <td>$glutenfree</td>
       </tr>
       <tr>
         <td>Cost in total</td>
-        <td></td>
+        <td>£$total</td>
       </tr>
     </table>
+    <br>
+    <a href="index.php">Back to order</a> 
 END;
 ?>
 </body>
