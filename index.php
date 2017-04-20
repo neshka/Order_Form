b98ac6069e0280e5985341182b7e9799078c1904
neshka <neshka_mind@yahoo.com>
2017-04-20 20:14:13 +0100

    add order form

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
+    <title>Bakery</title>
 </head>
 <body>
-<?php
-$name="Agnieszka";
-echo 'Hello '.$name.'!';
-?>
+<h1>You can order here your favourite donuts!</h1>
+<h3>How many of them you wish to order?</h3>
+Classic £0.50<br>
+<input type="text">
+<br><br>
+Gluten free £0.75<br>
+<input type="text">
+<br><br>
+<input type="submit">
 </body>
 </html>
 
